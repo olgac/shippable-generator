7b2fe2f21facdd49270c8541bdd4fd5d68f386d7
Ahmet Vehbi Olgaç <ahmet.olgac@eftsoftware.com>
2017-07-06 20:42:10 +0300

    init

---
+++ generate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ROOT_PATH=$(dirname "$0")
+if [ ! -f $ROOT_PATH/applications.list ]; then
+    echo "$ROOT_PATH/applications.list File not found!"
+    exit 0
+fi
+
+if [ ! -d $ROOT_PATH/templates ]; then
+    echo "$ROOT_PATH/templates Folder not found!"
+    exit 0
+fi
+
+echo "$(date) STARTED"
+NOW=$(date +%Y%m%d_%H%M%S)
+mkdir -p $ROOT_PATH/$NOW
+
+LOWER_DASH_KEY="XlowerdashapplicatonnameX"
+LOWER_UNDERSCORE_KEY="XlowerunderscoreapplicatonnameX"
+UPPER_UNDERSCORE_KEY="XUPPERUNDERSCOREAPLICATIONNAMEX"
+
+for APLICATION_NAME in $(cat $ROOT_PATH/applications.list); do
+    mkdir -p $ROOT_PATH/$NOW/$APLICATION_NAME
+    LOWER_UNDERSCORE=$(echo $APLICATION_NAME | sed 's/\-/_/g')
+    UPPER_UNDERSCORE=$(echo $LOWER_UNDERSCORE | tr /a-z/ /A-Z/)
+    echo $APLICATION_NAME $LOWER_UNDERSCORE $UPPER_UNDERSCORE
+
+    for TEMPLATE_FILE in $ROOT_PATH/templates/*; do
+        sed "s/${LOWER_DASH_KEY}/${APLICATION_NAME}/g; s/${LOWER_UNDERSCORE_KEY}/${LOWER_UNDERSCORE}/g; s/${UPPER_UNDERSCORE_KEY}/${UPPER_UNDERSCORE}/g;" $TEMPLATE_FILE > $ROOT_PATH/$NOW/$APLICATION_NAME/$(basename $TEMPLATE_FILE).txt
+    done;
+done
+
+echo "$(date) FINISHED"
